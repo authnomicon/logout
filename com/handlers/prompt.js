e47449d2db081cefb037604035754ccf4b7e5a7b
Jared Hanson <jaredhanson@gmail.com>
2023-11-28 11:33:00 -0800

    Add documentation.

--- com/handlers/prompt.js
+++ com/handlers/prompt.js
@@ -2,12 +2,24 @@
 var path = require('path')
   , ejs = require('ejs');
 
+/**
+ * Create logout prompt handler.
+ *
+ * Returns a handler that prompts the user to logout.
+ *
+ * In most situations, the logout prompt is not used, as logout requests will be
+ * submitted directly from the application to the `POST /logout` endpoint.
+ * However, in some circumstances, such as single logout (SLO), logout will be
+ * initiated by another application.  In this case, if the logout request cannot
+ * be verified, the user may be prompted to confirm their intent to logout.
+ * This confirmation prevents denial of service attacks.
+ */
 exports = module.exports = function(store) {
   
   function prompt(req, res, next) {
     // If a CSRF token is supplied, invoke the next route (which is expected to
     // be the action handler).  This allows the application to issue
-    // `GET /logout` requests via redirection and terminate the login session.
+    // `GET /logout` requests via redirection which terminate the login session.
     // This is particularly useful for implementing single logout (SLO), and is
     // utilized by related packages, such as `@authnomicon/openidconnect`.
     if (req.query.csrf_token) { return next('route'); }
