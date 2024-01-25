81fe8a230d92aff344c766e9e4dedef72a4c1294
Jared Hanson <jaredhanson@gmail.com>
2024-01-25 11:04:55 -0800

    Add tests.

--- test/prompt.test.js
+++ test/prompt.test.js
@@ -9,6 +9,11 @@ var factory = require('../com/prompt');
 
 describe('prompt', function() {
   
+  it('should be annotated', function() {
+    expect(factory['@implements']).to.deep.equal('http://i.authnomicon.org/prompts/http/Prompt');
+    expect(factory['@name']).to.equal('logout');
+  });
+  
   it('should create handler', function() {
     var handler = factory();
     expect(handler).to.be.a('function');
